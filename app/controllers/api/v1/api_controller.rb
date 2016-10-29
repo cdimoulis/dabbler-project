b468a900cf70fe4e04d325a426825dbc08764802
Chris Dimoulis <christopherjamesd@gmail.com>
2016-10-29 15:16:11 -0500

    [ADDED] Api V1 and Domain Controllers

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,32 @@
+#
+# Parent controller for all API V1 controllers
+#
+class Api::V1::ApiController < ActionController::Base
+
+  ###
+  # Standard CRUD Ops
+  ###
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+  ###
+  # End standard CRUD Ops
+  ###
+
+end
---
+++ app/controllers/api/v1/domains_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::DomainsController < ApplicationController
+
+  ###
+  # Standard CRUD Ops
+  ###
+  def create
+    super
+  end
+
+  def index
+    super
+  end
+
+  def show
+    super
+  end
+
+  def update
+    super
+  end
+
+  def delete
+    super
+  end
+  ###
+  # End standard CRUD Ops
+  ###
+
+end
