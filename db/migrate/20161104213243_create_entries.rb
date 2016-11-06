dee9eba90098c64243a14973a0993c772e07f1a7
Chris Dimoulis <christopherjamesd@gmail.com>
2016-11-06 11:44:53 -0600

    [REMOVED] Entry model/controller/tests. Doing other models first

--- db/migrate/20161104213243_create_entries.rb
+++
@@ -1,8 +0,0 @@
-class CreateEntries < ActiveRecord::Migration
-  def change
-    create_table :entries, id: :uuid, default: "uuid_generate_v4()", force: true do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
