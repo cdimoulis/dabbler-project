baafc397d0c4dd409600823626fd58131f757740
Chris Dimoulis <christopherjamesd@gmail.com>
2016-11-10 14:12:15 -0600

    [ADDED] People controller, moved controller functions to concerns #5

---
+++ app/controllers/concerns/page_records.rb
@@ -0,0 +1,17 @@
+module PageRecords
+  extend ActiveSupport::Concern
+
+  private
+
+    # Handle record paging
+    # Params:
+    #     start -> Starting record
+    #     count -> Number of records to fetch
+    def pageRecords
+      start = params.has_key?(:start) ? params[:start] : 0
+      count = params.has_key?(:count) ? params[:count] : 1000
+
+      @records = @records.offset(start).limit(count)
+    end
+
+end
