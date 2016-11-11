e6f3d1a92413eb60f337905927a09bf07489d8b5
Chris Dimoulis <christopherjamesd@gmail.com>
2016-11-11 10:27:15 -0600

    {CHANGED] Domain factory. better understanding #4

--- spec/factories/domains.rb
+++ spec/factories/domains.rb
@@ -1,29 +1,11 @@
 # Factory for
 #   Domain model
 FactoryGirl.define do
+
+  factory :domain do
+    text "Empty"
+    description { "#{text} domain" }
+    subdomain { text.downcase }
+  end
   
-  factory :domain_travel, class: Domain do
-    text 'Travel'
-    description 'Travel related Information'
-    subdomain 'travel'
-  end
-
-  factory :domain_code, class: Domain do
-    text 'Code'
-    description 'Code related Information'
-    subdomain 'code'
-  end
-
-  factory :domain_inactive, class: Domain do
-    text 'Inactive'
-    description 'Inactive domain'
-    subdomain 'inactive'
-    active false
-  end
-
-  factory :domain_unused, class: Domain do
-    text 'Unused'
-    description 'Unused domain'
-    subdomain 'unused'
-  end
 end
