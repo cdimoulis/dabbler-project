52afc10ddc05554456be755a651ff537fa61f9a1
Chris Dimoulis <christopherjamesd@gmail.com>
2016-11-12 11:42:31 -0600

    [ADDED] Update tests for domain #4

--- spec/controllers/api/v1/domain_controller_spec.rb
+++ spec/controllers/api/v1/domain_controller_spec.rb
@@ -63,5 +63,24 @@ RSpec.describe Api::V1::DomainsController do
 
   end
 
+  # Test for UPDATE route
+  context "#update" do
+    # Allow travel to be shared across all tests
+    let!(:travel) {create(:domain, text: "Travel")}
+    let!(:code) {create(:domain, text: "Code")}
+
+    it "succeeds" do
+      update_params = {description: "Travel related entries"}
+      put :update, id: travel.id, domain: update_params, format: :json
+      expect(response).to have_http_status(:success)
+      expect(assigns(:record).description).to eq(update_params[:description])
+    end
+
+    it "prevents invalid updates" do
+      update_params = {text: "Code"}
+      put :update, id: travel.id, domain: update_params, format: :json
+      expect(response).to have_http_status(424)
+    end
+  end
 
 end
