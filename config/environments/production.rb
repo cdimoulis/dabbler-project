1354896301473e547ccc60e674350656fa52d777
Chris Dimoulis <christopherjamesd@gmail.com>
2016-11-28 10:31:19 -0600

    [REMOVED] All devise files. Added clearance gem #10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   #   :authentication       => "plain",
   #   :enable_starttls_auto => true
   # }
-  config.action_mailer.default_url_options = { host: 'localhost:5100' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  # config.action_mailer.default_url_options = { host: 'localhost:5100' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
