9442ea26b80a2c2793ce1673bd6064c473b02995
Chris Dimoulis <christopherjamesd@gmail.com>
2016-12-02 14:37:07 -0600

    [CHANGED] Password views, routes, locales #10

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Mailer Configure Options
-  config.action_mailer.default_url_options = { host: 'localhost:5100' }
+  config.action_mailer.default_url_options = { host: 'localhost:5000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   exc_new_edit = [:new, :edit]
 
   #####
-  # API Routes for the database models
+  # BLOG Routes for the database models
   #####
   namespace :blog do
     namespace :v1 do
@@ -22,12 +22,30 @@ Rails.application.routes.draw do
     end
   end
   #####
-  # END API Routes
+  # END BLOG Routes
   #####
 
+
+  #####
+  # Session Routes
+  #####
+  resource :session, controller: 'clearance/sessions', only: [:create, :destroy]
+
+  # TEMP SIGN IN FOR TESTING (actually so that sign out redirect properly)
+  # Either the url after sign out needs to change (currently it is sign_in below)
+  # or we just don't redirect (which will probably require extending the sessions
+  # controller)
+  get '/login' => 'application#login', as: 'sign_in'
+
+  resources :users, only: [], constraints: uuid_constraints do
+    resource :password, only: [:create, :edit, :update]
+  end
+
+  resources :passwords,
+    only: [:create, :new]
 end
 
-ADDITIONAL_ROUTES = Dir[Rails.root.join('config','routes','**','*.rb')]
-ADDITIONAL_ROUTES.each do |route_file|
-  require route_file
-end
+# ADDITIONAL_ROUTES = Dir[Rails.root.join('config','routes','**','*.rb')]
+# ADDITIONAL_ROUTES.each do |route_file|
+#   require route_file
+# end
