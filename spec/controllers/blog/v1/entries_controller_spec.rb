93e2fac3d8a1df7bf8506411b81b5a806b66f834
Chris Dimoulis <christopherjamesd@gmail.com>
2016-12-15 10:37:55 -0600

    [ADDED] Show and update specs #2

--- spec/controllers/blog/v1/entries_controller_spec.rb
+++ spec/controllers/blog/v1/entries_controller_spec.rb
@@ -54,6 +54,38 @@ RSpec.describe Blog::V1::EntriesController do
 
   # Tests for SHOW route
   context "#show" do
+    # Allow travel to be shared across all tests
+    let!(:entry) { create(:entry) }
 
+    # Before running a test do this
+    before do
+      get :show, id: entry.id, format: :json
+    end
+
+    it { is_expected.to respond_with(:success) }
+
+    it 'returns JSON' do
+      # look_like_json found in support/matchers/json_matchers.rb
+      expect(response.body).to look_like_json
+    end
+
+    it { expect(assigns(:record)).to eq(entry) }
+  end
+
+  # Test for UPDATE route
+  context "#update" do
+    before do
+      sign_in
+    end
+
+    # Allow travel to be shared across all tests
+    let!(:entry) { create(:entry) }
+
+    it "succeeds" do
+      update_params = {description: "Some new information"}
+      put :update, id: code.id, domain: update_params, format: :json
+      expect(response).to have_http_status(:success)
+      expect(assigns(:record).description).to eq(update_params[:description])
+    end
   end
 end
