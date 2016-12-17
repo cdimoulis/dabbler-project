fc54430e36e4874784c2e60ab7d7bd8bcea064ac
Chris Dimoulis <christopherjamesd@gmail.com>
2016-12-17 15:32:37 -0600

    [ADDED] User->Entry association specs #2

--- spec/requests/blog/v1/users_controller_spec.rb
+++ spec/requests/blog/v1/users_controller_spec.rb
@@ -4,11 +4,39 @@ RSpec.describe Blog::V1::UsersController do
   include RequestSpecHelper
 
   context '#entries' do
+    let!(:author) { create(:user) }
+    let!(:entry_a) { create(:entry, text: "Entry A", author: author, creator: author) }
+    let!(:entry_b) { create(:entry, text: "Entry B", author: author, creator: author) }
+    let!(:entry_c) { create(:entry, text: "Entry C", author: author, creator: author) }
+    let!(:entry_d) { create(:entry_with_creator, text: "Entry D") }
+    let(:entries_via_user_path) { blog_v1_user_entries_path(user_id: author.id) }
 
     it 'returns correct entries for author' do
-
+      get entries_via_user_path, format: :json
+      expect(assigns(:records).length).to eq(3)
+      order = [entry_a.id, entry_b.id, entry_c.id]
+      expect(assigns(:records).pluck('id')).to match(order)
     end
+  end
 
+  context '#contributions' do
+    let!(:author) { create(:user) }
+    let!(:entry_a) { create(:entry_with_creator, text: "Entry A") }
+    let!(:entry_b) { create(:entry_with_creator, text: "Entry B") }
+    let!(:entry_c) { create(:entry_with_creator, text: "Entry C") }
+    let!(:entry_d) { create(:entry_with_creator, text: "Entry D") }
+    let(:contributions_via_user_path) { blog_v1_user_contributions_path(user_id: author.id) }
+
+    it 'returns correct entries for author' do
+      entry_b.contributors << author
+      entry_c.contributors << author
+      entry_d.contributors << author
+
+      get contributions_via_user_path, format: :json
+      expect(assigns(:records).length).to eq(3)
+      order = [entry_b.id, entry_c.id, entry_d.id]
+      expect(assigns(:records).pluck('id')).to match(order)
+    end
   end
 
 end
