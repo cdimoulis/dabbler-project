86e0ea4d690c2405ae7e34997e115d70cf24cdc4
Chris Dimoulis <christopherjamesd@gmail.com>
2017-02-20 11:44:33 -0600

    [ADDED] MenusMenuGroup specs #47

--- spec/factories/menus_menu_groups.rb
+++ spec/factories/menus_menu_groups.rb
@@ -11,5 +11,7 @@ FactoryGirl.define do
   factory :menus_menu_group do
     menu { create(:menu) }
     menu_id { menu.present? ? menu.id : nil }
+    menu_group { menu.present? ? create(:menu_group, domain: menu.domain) : create(:menu_group) }
+    menu_group_id { menu_group.present? ? menu_group.id : nil }
   end
 end
--- spec/models/menus_menu_group_spec.rb
+++ spec/models/menus_menu_group_spec.rb
@@ -10,5 +10,28 @@
 require 'rails_helper'
 
 RSpec.describe MenusMenuGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'validations' do
+    it 'requires unique order' do
+      mmg = create(:menus_menu_group)
+      menu_group = create(:menu_group, domain: mmg.menu.domain, order: mmg.menu_group.order)
+      mmg_new = build(:menus_menu_group, menu_group: menu_group)
+      expect(mmg_new.valid?).to be_falsy
+    end
+
+    it 'unique order only applies to type' do
+      mmg = create(:menus_menu_group)
+      menu = create(:menu, domain: mmg.menu.domain)
+      menu_group = create(:menu_group, domain: mmg.menu.domain, order: mmg.menu_group.order)
+      mmg_new = build(:menus_menu_group, menu: menu, menu_group: menu_group)
+      expect(mmg_new.valid?).to be_truthy
+    end
+
+    it 'requires domain to match' do
+      menu = create(:menu)
+      group = create(:menu_group)
+      mmg = build(:menus_menu_group, menu: menu, menu_group: group)
+      expect(mmg.valid?).to be_falsy
+    end
+  end
 end
