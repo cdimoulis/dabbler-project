1bd2c899aa74e34b8decf503f2b9ce769171bc92
Chris Dimoulis <christopherjamesd@gmail.com>
2017-04-19 16:44:00 -0500

    [ADDED] Missing specs for menu model #45

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -17,7 +17,8 @@ RSpec.describe Menu, type: :model do
 
   context 'associations' do
     it { is_expected.to belong_to(:domain) }
-    it { is_expected.to have_many(:menu_groups) }
+    it { is_expected.to have_many(:menus_menu_groups) }
+    it { is_expected.to have_many(:menu_groups).through(:menus_menu_groups) }
 
     it 'removes menu_groups join on destroy' do
       menu = create(:menu)
@@ -43,25 +44,30 @@ RSpec.describe Menu, type: :model do
     it 'fails with duplicate text' do
       menu_a = create(:menu, text: 'Reviews')
       menu_b = build(:menu, text: 'Reviews', domain: menu_a.domain)
-      expect(menu_b.save).to be_falsy
+      expect(menu_b.valid?).to be_falsy
     end
 
     it 'allows duplicate text in different domain' do
       menu_a = create(:menu, text: 'Reviews')
       menu_b = build(:menu, text: 'Reviews')
-      expect(menu_b.save).to be_truthy
+      expect(menu_b.valid?).to be_truthy
+    end
+
+    it 'requires a domain' do
+      menu = build(:menu, domain: nil)
+      expect(menu.valid?).to be_falsy
     end
 
     it 'fails with duplicate order' do
       menu_a = create(:menu)
       menu_b = build(:menu, order: menu_a.order, domain: menu_a.domain)
-      expect(menu_b.save).to be_falsy
+      expect(menu_b.valid?).to be_falsy
     end
 
     it 'allows duplicate order in different domain' do
       menu_a = create(:menu)
       menu_b = build(:menu, order: menu_a.order)
-      expect(menu_b.save).to be_truthy
+      expect(menu_b.valid?).to be_truthy
     end
   end
 end
