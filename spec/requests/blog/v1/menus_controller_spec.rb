c1e097490109e3835a844ebbdf12dad4cda38d96
Chris Dimoulis <christopherjamesd@gmail.com>
2017-08-16 16:04:14 -0500

    [ADDED] Menu single_index spec under menu_group #51

--- spec/requests/blog/v1/menus_controller_spec.rb
+++ spec/requests/blog/v1/menus_controller_spec.rb
@@ -58,6 +58,34 @@ RSpec.describe Blog::V1::MenusController do
     end
   end
 
+  context '#single_index' do
+    let!(:menu) { create(:menu) }
+
+    it 'returns correct menu for menu_group' do
+      menu_group = create(:menu_group, menu: menu)
+      get blog_v1_menu_group_menu_path(menu_group_id: menu_group.id), format: :json
+      expect(assigns(:record)).to eq(menu)
+    end
+
+    # it 'returns correct menu for topic' do
+    #   topic = create(:topic_with_domain, domain_id: domain.id)
+    #   get blog_v1_topic_domain_path(topic_id: topic.id), format: :json
+    #   expect(assigns(:record)).to eq(domain)
+    # end
+    #
+    # it 'returns correct menu for published_entry' do
+    #   published_entry = create(:published_entry, domain: domain)
+    #   get blog_v1_published_entry_domain_path(published_entry_id: published_entry.id), format: :json
+    #   expect(assigns(:record)).to eq(domain)
+    # end
+    #
+    # it 'returns correct menu for featured_entry' do
+    #   featured_entry = create(:featured_entry, domain: domain)
+    #   get blog_v1_featured_entry_domain_path(featured_entry_id: featured_entry.id), format: :json
+    #   expect(assigns(:record)).to eq(domain)
+    # end
+  end
+
   # Valid child ordering attribute
   context "VALID_CHILD_ORDERING_ATTRIBUTES" do
     let!(:menu) { create(:menu) }
